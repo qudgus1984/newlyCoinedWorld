5e8f6d73a767cdaefa17a6225117dc71067cbe3d
ByeongHyeon <qudgus1984@naver.com>
2022-07-08 21:17:38 +0900

    Initial Commit

---
+++ newlyCoinedWorld/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  newlyCoinedWorld
+//
+//  Created by 이병현 on 2022/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var newlyButton1: UIButton!
+    @IBOutlet weak var newlyButton2: UIButton!
+    @IBOutlet weak var newlyButton3: UIButton!
+    @IBOutlet weak var newlyButton4: UIButton!
+    @IBOutlet weak var appearTextLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func newlyButtonClicked(_ sender: UIButton) {
+    }
+
+    
+    func newlyButton(newlyButtonName: UIButton) {
+        
+    }
+}
+
