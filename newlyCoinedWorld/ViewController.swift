24a39efeb9b11ed9924c104e33a314d1a343ca10
ByeongHyeon <qudgus1984@naver.com>
2022-07-13 16:12:46 +0900

    신조어 함수 안에 저장 후 호출 형태로 변경

--- newlyCoinedWorld/ViewController.swift
+++ newlyCoinedWorld/ViewController.swift
@@ -49,6 +49,7 @@ class ViewController: UIViewController {
         
     }
     
+    
     // 연관 버튼 신조어 해석
     @IBAction func newlyButton1Clicked(_ sender: UIButton) {
         appearTextLabel.text = "구독자 유뮤를 차별한다는 뜻"
@@ -67,37 +68,11 @@ class ViewController: UIViewController {
     @IBAction func searchButton(_ sender: UIButton) {
         // 신조어 추가
         
-        let newRawValue = NewlyCoinedWorld(rawValue: searchTextField.text!)
-        switch newRawValue {
-            
-        case .윰차:
-            appearTextLabel.text = "구독자 유뮤를 차별한다는 뜻"
-        case .실매:
-            appearTextLabel.text = "실시간 매니저를 줄인 말"
-        case .만반잘부:
-            appearTextLabel.text = "만나서 반가워 잘 부탁해! 의 줄임말"
-        case .꾸안꾸:
-            appearTextLabel.text = "꾸민듯 안꾸민듯의 줄임말"
-        case .배사:
-            appearTextLabel.text = "배경사진의 줄임 말"
-        case .디엠:
-            appearTextLabel.text = "다이렉트 메세지의 줄임 말"
-        case .구취:
-            appearTextLabel.text = "구독 취소의 줄임 말"
-        case .떡상:
-            appearTextLabel.text = "크게 상승한다의 줄임 말"
-        case .떡락:
-            appearTextLabel.text = "크게 하락한다의 줄임 말"
-        case .none:
-            appearTextLabel.text = "값 없음"
-        }
-        // 왜 다른 함수 호출이 안될까... 왜... -> 나는 할수있다아아!!!
-
+        newWolrd(rawValue: searchTextField.text!)
         // 검색 버튼 눌렀을 때 키보드 내리기
         view.endEditing(true)
         
     }
-    
     
     // 뷰 클릭시 키보드 내리기
     @IBAction func tapGestureView(_ sender: UITapGestureRecognizer) {
@@ -115,7 +90,11 @@ class ViewController: UIViewController {
     @IBAction func searchTextFieldClicked(_ sender: UITextField) {
     }
     @IBAction func tapReturnKey(_ sender: UITextField) {
-        let newRawValue = NewlyCoinedWorld(rawValue: sender.text!)
+        newWolrd(rawValue: sender.text!)
+    }
+    
+    func newWolrd(rawValue: String) {
+        let newRawValue = NewlyCoinedWorld(rawValue: rawValue)
         switch newRawValue {
             
         case .윰차:
@@ -141,6 +120,9 @@ class ViewController: UIViewController {
         }
     }
     
+    
 }
+
+
 
 
